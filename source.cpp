ed9b5d4ab5fc83f4c9334d6185f7c5f6622971ca
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-07-31 23:33:02 +0200

    Already done solution for Variable-Sized-Arrays problem

--- source.cpp
+++ source.cpp
@@ -1,26 +1,42 @@
-﻿#include <cstdio>
+﻿#include <cmath>
+#include <cstdio>
 #include <vector>
 #include <iostream>
 #include <algorithm>
-using namespace std;
 
-void update(int* a, int* b)
-{
-    int temp1 = *a + *b;
-    int temp2 = abs(*a - *b);
-    *a = temp1;
-    *b = temp2;
-}
 
 int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    std::vector<std::vector<int>> Index;
+    int NumberOfIndex = 0;
+    int NumberOfQueries = 0;
+    int sizeOfVec;
+    int counter = 0;
+    int counter2 = 0;
+    int a;
 
-    int c;
-    int d;
-    int* a = &c;
-    int* b = &d;
+    std::cin >> NumberOfIndex;
+    std::cin >> NumberOfQueries;
 
-    cin >> c >> d;
-    update(a, b);
-    cout << *a << endl << *b;
+
+    for (int i = 0; i < NumberOfIndex; i++) {
+
+        Index.push_back(std::vector<int>());
+        std::cin >> sizeOfVec;
+
+        for (int j = 0; j < sizeOfVec; j++) {
+            std::cin >> a;
+            Index[i].push_back(a);
+        }
+    }
+
+    int index;
+    int querry;
+
+    while (NumberOfQueries > counter2) {
+        std::cin >> index >> querry;
+        std::cout << Index[index][querry] << std::endl;
+        counter2++;
+    }
     return 0;
 }
