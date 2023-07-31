c2142c92e32dd4dc8ca007d00c533c038da25e59
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-07-31 23:26:02 +0200

    Already done solution for Pointers problem

--- source.cpp
+++ source.cpp
@@ -1,35 +1,26 @@
-﻿#include <cmath>
-#include <cstdio>
+﻿#include <cstdio>
 #include <vector>
 #include <iostream>
 #include <algorithm>
+using namespace std;
 
+void update(int* a, int* b)
+{
+    int temp1 = *a + *b;
+    int temp2 = abs(*a - *b);
+    *a = temp1;
+    *b = temp2;
+}
 
 int main() {
-    std::vector<int> numbers;
-    int size;
-    int nQueries;
 
-    std::cin >> size;
-    for (int i = 0; i < size; i++) {
-        int temp;
-        std::cin >> temp;
-        numbers.push_back(temp);
-    }
-    std::cin >> nQueries;
+    int c;
+    int d;
+    int* a = &c;
+    int* b = &d;
 
-    for (int i = 0; i < nQueries; i++) {
-        int temp = 0;
-        std::cin >> --temp;
-        auto point = std::lower_bound(numbers.begin(), numbers.end(), temp);
-        // if number that we typed occures then we print Yes and it's index location(pos in vector)
-        // if the number is not present you have to print "No" (without the quotes) followed by the
-        // index of the next smallest number just greater than that number.
-
-        if (temp == *point)
-            std::cout << "Yes " << ++point - numbers.begin() << std::endl;
-        else
-            std::cout << "No " << ++point - numbers.begin() << std::endl;
-    }
+    cin >> c >> d;
+    update(a, b);
+    cout << *a << endl << *b;
     return 0;
 }
