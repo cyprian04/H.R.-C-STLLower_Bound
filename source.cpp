a86938d9fc35168db7a73e933c8eb88f87f3337f
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-07-31 23:54:24 +0200

    Already done solution for Structs

--- source.cpp
+++ source.cpp
@@ -3,33 +3,21 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+using namespace std;
+
+
+struct PersonalData {
+    int value1;
+    int value2;
+    std::string valueN;
+    std::string valueS;
+};
 
 
 int main() {
-    std::vector<int> numbers;
-    int size;
-    int nQueries;
 
-    std::cin >> size;
-    for (int i = 0; i < size; i++) {
-        int temp;
-        std::cin >> temp;
-        numbers.push_back(temp);
-    }
-    std::cin >> nQueries;
-
-    for (int i = 0; i < nQueries; i++) {
-        int temp = 0;
-        std::cin >> --temp;
-        auto point = std::lower_bound(numbers.begin(), numbers.end(), temp);
-        // if number that we typed occures then we print Yes and it's index location(pos in vector)
-        // if the number is not present you have to print "No" (without the quotes) followed by the
-        // index of the next smallest number just greater than that number.
-
-        if (temp == *point)
-            std::cout << "Yes " << ++point - numbers.begin() << std::endl;
-        else
-            std::cout << "No " << ++point - numbers.begin() << std::endl;
-    }
+    PersonalData dt;
+    cin >> dt.value1 >> dt.valueN >> dt.valueS >> dt.value2;
+    cout << dt.value1 << " " << dt.valueN << " " << dt.valueS << " " << dt.value2;
     return 0;
 }
