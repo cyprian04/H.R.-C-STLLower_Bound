208d345e6420cc7d3af25556735ec77f9c7f6beb
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-08-01 16:42:04 +0200

    Already done solution for StringStream problem

--- source.cpp
+++ source.cpp
@@ -3,33 +3,28 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <sstream>
+using namespace std;
 
+void parseInts(string str) {
+
+    stringstream ss(str);
+    vector<int> liczby;
+    char ch;
+    int temp;
+
+    while (ss) {
+        ss >> temp >> ch;
+        liczby.push_back(temp);
+    }
+
+    for (int i = 0; i < int(liczby.size()); i++)
+        cout << liczby[i] << endl;
+}
 
 int main() {
-    std::vector<int> numbers;
-    int size;
-    int nQueries;
-
-    std::cin >> size;
-    for (int i = 0; i < size; i++) {
-        int temp;
-        std::cin >> temp;
-        numbers.push_back(temp);
-    }
-    std::cin >> nQueries;
-
-    for (int i = 0; i < nQueries; i++) {
-        int temp = 0;
-        std::cin >> --temp;
-        auto point = std::lower_bound(numbers.begin(), numbers.end(), temp);
-        // if number that we typed occures then we print Yes and it's index location(pos in vector)
-        // if the number is not present you have to print "No" (without the quotes) followed by the
-        // index of the next smallest number just greater than that number.
-
-        if (temp == *point)
-            std::cout << "Yes " << ++point - numbers.begin() << std::endl;
-        else
-            std::cout << "No " << ++point - numbers.begin() << std::endl;
-    }
+    string ABC;
+    cin >> ABC;
+    parseInts(ABC);
     return 0;
 }
