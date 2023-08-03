d2e80bf67301c386026fd90120a405bd9166f768
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-08-03 18:53:42 +0200

    Already done solution for Print-Pretty problem

--- source.cpp
+++ source.cpp
@@ -1,35 +1,19 @@
-﻿#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-
+﻿#include <iostream>
+#include <iomanip> 
+using namespace std;
 
 int main() {
-    std::vector<int> numbers;
-    int size;
-    int nQueries;
+	int T; cin >> T;
+	cout << setiosflags(ios::uppercase);
+	cout << setw(0xf) << internal;
+	while (T--) {
+		double A; cin >> A;
+		double B; cin >> B;
+		double C; cin >> C;
+		cout << noshowpos << left << hex << showbase << nouppercase << (long long)(A) << endl;
+		cout << showpos << dec << setw(15) << setfill('_') << right << fixed << setprecision(2) << B << endl;
+		cout << noshowpos << left << scientific << setprecision(9) << uppercase << C << endl;
+	}
+	return 0;
 
-    std::cin >> size;
-    for (int i = 0; i < size; i++) {
-        int temp;
-        std::cin >> temp;
-        numbers.push_back(temp);
-    }
-    std::cin >> nQueries;
-
-    for (int i = 0; i < nQueries; i++) {
-        int temp = 0;
-        std::cin >> --temp;
-        auto point = std::lower_bound(numbers.begin(), numbers.end(), temp);
-        // if number that we typed occures then we print Yes and it's index location(pos in vector)
-        // if the number is not present you have to print "No" (without the quotes) followed by the
-        // index of the next smallest number just greater than that number.
-
-        if (temp == *point)
-            std::cout << "Yes " << ++point - numbers.begin() << std::endl;
-        else
-            std::cout << "No " << ++point - numbers.begin() << std::endl;
-    }
-    return 0;
 }
