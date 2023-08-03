c7fbcbbbc7be6a3d5d4821e35e9a46dca6ca0c1f
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-08-03 17:59:50 +0200

    Already done solution for Preprocessor_Solution problem(C++20 version of problem)

--- source.cpp
+++ source.cpp
@@ -4,32 +4,19 @@
 #include <iostream>
 #include <algorithm>
 
+#define MaxMin( a, b) (std::abs(*(a)) + std::abs(*(b))) // adding * so we get value of iterator
 
 int main() {
-    std::vector<int> numbers;
-    int size;
-    int nQueries;
 
+    int size, val, result;
     std::cin >> size;
+    std::vector<int> vec;
+
     for (int i = 0; i < size; i++) {
-        int temp;
-        std::cin >> temp;
-        numbers.push_back(temp);
+        std::cin >> val;
+        vec.push_back(val);
     }
-    std::cin >> nQueries;
-
-    for (int i = 0; i < nQueries; i++) {
-        int temp = 0;
-        std::cin >> --temp;
-        auto point = std::lower_bound(numbers.begin(), numbers.end(), temp);
-        // if number that we typed occures then we print Yes and it's index location(pos in vector)
-        // if the number is not present you have to print "No" (without the quotes) followed by the
-        // index of the next smallest number just greater than that number.
-
-        if (temp == *point)
-            std::cout << "Yes " << ++point - numbers.begin() << std::endl;
-        else
-            std::cout << "No " << ++point - numbers.begin() << std::endl;
-    }
+    result = MaxMin(std::max_element(vec.begin(), vec.end()), std::min_element(vec.begin(), vec.end())); // passing iterators of max and min element
+    std::cout << "Result = " << result;
     return 0;
 }
